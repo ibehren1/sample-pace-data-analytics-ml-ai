bc0e2c9a0e4390d35494b79dfe040b34da46f267
Isaac Behrens <ibehr@amazon.com>
2025-04-15 13:09:38 -0500

    Correct backend.tf region to match  rather than .

--- iac/roots/idc/idc-org/backend.tf
+++ iac/roots/idc/idc-org/backend.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
     bucket         = "###TF_S3_BACKEND_NAME###-###AWS_ACCOUNT_ID###-###AWS_DEFAULT_REGION###"
     dynamodb_table = "###TF_S3_BACKEND_NAME###-lock"
-    region         = "###AWS_DEFAULT_REGION###"
+    region         = "###AWS_PRIMARY_REGION###"
     key            = "###ENV_NAME###/idc-org/terraform.tfstate"
     encrypt        = true
   }
